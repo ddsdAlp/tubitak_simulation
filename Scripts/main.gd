96b155c2dbf5e95881d4f44833ebd9793d54c9e6
Alp Çetintaş <alp35cetintas@gmail.com>
2024-12-05 15:18:41 +0300

    v0.1 - Added UI elements
    -Added 4 Sliders to change the value of simulation variables
    -Added a pause simulation button

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -2,15 +2,17 @@ extends Node2D
 
 var atoms = []
 var simulation_speed: float = 1.0
+var simulation_state = true
 
 # Preload the atom scene once
-const ATOM_SCENE = preload("res://atom.tscn")
+const ATOM_SCENE = preload("res://Scenes/atom.tscn")
 
 func _ready():
 	# start the simulation
 	set_process(true)
 
 func _process(delta):
+	handle_labels()
 	if atoms.size() > 0:
 		simulate()
 
@@ -67,9 +69,39 @@ func _input(event):
 		atom_type = "Fe"
 	
 	
-	if Input.is_action_just_pressed("left click"):
+	if Input.is_action_just_pressed("space"):
 		var new_atom = ATOM_SCENE.instantiate()
 		new_atom.update_atom(atom_type)
 		new_atom.position = get_global_mouse_position()
 		add_child(new_atom)
 		atoms.append(new_atom)
+
+
+
+# handle signals
+func _on_attraction_range_value_changed(value):
+	attraction_threshold = value
+
+func _on_attraction_strength_value_changed(value):
+	attraction_strength = value
+
+func _on_repulsion_range_value_changed(value):
+	repulsion_threshold = value
+
+func _on_repulsion_strength_value_changed(value):
+	repulsion_strength = value
+
+func _on_pause_button_pressed():
+	get_tree().paused = !get_tree().paused
+
+# handle labels
+@onready var attraction_range_label = $user_interface/VBoxContainer/VBoxContainer/attraction_range_label
+@onready var attraction_strength_label = $user_interface/VBoxContainer/VBoxContainer2/attraction_strength_label
+@onready var repulsion_range_label = $user_interface/VBoxContainer/VBoxContainer3/repulsion_range_label
+@onready var repulsion_strength_label = $user_interface/VBoxContainer/VBoxContainer4/repulsion_strength_label
+
+func handle_labels():
+	attraction_range_label.text = "Attraction Range: " + str(attraction_threshold)
+	attraction_strength_label.text = "Attraction Strength: " + str(attraction_strength)
+	repulsion_range_label.text = "Repulsion Range: " + str(repulsion_threshold)
+	repulsion_strength_label.text = "Repulsion Strength: " + str(repulsion_strength)
